759af62a518d1dc346c42f343a2590deaca17344
k4ndAr3c <k4ndAr3c@github.com>
2016-04-04 18:58:00 +0200

    put .php

--- index.php
+++ index.php
@@ -7,6 +7,14 @@
     </head>
     <body>
         <h1 align=right> Hello World! </h1>
+<?php
+$date = date('Y-m-d H:i:s');
+$ua = $_SERVER['HTTP_USER_AGENT'];
+$ip=$_SERVER['REMOTE_ADDR'];
+$f = fopen ("cons", "a");
+fputs($f, "$ua\t$ip\t$date\n");
+fclose($f);
+?>
 <div align=center><p>Voici un petit jeu, have fun .!.</p></div>
 <br /><script type="text/javascript" src="js.js"></script>
     </body>
