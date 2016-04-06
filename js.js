7b5df27ba41da092bb8eb54791a76bbef6f8549e
k4ndAr3c <k4ndAr3c@github.com>
2016-04-06 12:23:09 +0200

    update

--- js.js
+++ js.js
@@ -1,34 +1,36 @@
-var userChoice = prompt("Do you choose rock, paper or scissors?");
-var computerChoice = Math.random();
-
-if (computerChoice < 0.34) {
-    computerChoice = "rock";
-} else if(computerChoice <= 0.67) {
-    computerChoice = "paper";
-} else {
-    computerChoice = "scissors";
+function shifumi(){
+	var userChoice = prompt("Do you choose rock, paper or scissors?");
+	var computerChoice = Math.random();
+	
+	if (computerChoice < 0.34) {
+	    computerChoice = "rock";
+	} else if(computerChoice <= 0.67) {
+	    computerChoice = "paper";
+	} else {
+	    computerChoice = "scissors";
+	}
+	document.getElementById("choice").innerHTML = "<div align=middle>Computer: <b id=cchoice>" + computerChoice + "</b></div><br />";
+	
+	function compare(choice1, choice2){
+	    if (choice1 == choice2){
+	        return(">The result is a tie!");
+	    }else if (choice1 == "rock"){
+	        if (choice2 == "scissors"){
+	            return("id=win>YOU win");}
+	        else {
+	            return("id=loose>HE wins");}
+	    }else if (choice1 == "paper"){
+	        if (choice2 == "rock"){
+	            return("id=win>YOU win");}
+	        else {
+	            return("id=loose>HE wins");}
+	    }else if (choice1 == "scissors"){
+	        if (choice2 == "paper"){
+	            return("id=win>YOU win");}
+	        else {
+	            return("id=loose>HE wins");}
+	    }
+	}
+	document.getElementById("results").innerHTML = "<p align=center "+compare(userChoice, computerChoice)+"</p>" ;
+	return
 }
-document.write("<div align=middle>Computer: <b id=cchoice>" + computerChoice + "</b></div><br />");
-
-function compare(choice1, choice2){
-    if (choice1 == choice2){
-        return(">The result is a tie!");
-    }else if (choice1 == "rock"){
-        if (choice2 == "scissors"){
-            return("id=win>YOU win");}
-        else {
-            return("id=loose>HE wins");}
-    }else if (choice1 == "paper"){
-        if (choice2 == "rock"){
-            return("id=win>YOU win");}
-        else {
-            return("id=loose>HE wins");}
-    }else if (choice1 == "scissors"){
-        if (choice2 == "paper"){
-            return("id=win>YOU win");}
-        else {
-            return("id=loose>HE wins");}
-    }
-}
-document.write("<p align=center "+compare(userChoice, computerChoice)+"</p>");
-
