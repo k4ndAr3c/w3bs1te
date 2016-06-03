8746df331e3e47094e83cdb9db55d8f451dda8fd
k4ndAr3c <k4ndAr3c@github.com>
2016-06-03 16:01:26 +0200

    replay image

--- js.js
+++ js.js
@@ -361,6 +361,7 @@ function showSongs(grou){
 	else if (grou == "finntroll"){g = ["1", "Blodsvept", "3", "4"];}
 	else if (grou == "horna"){g = ["Hornanvaki", "Deux"];}
         else if (grou == "kronos"){g = ["Supreme Nordik Reign"];}
+        else if (grou == "ntm"){g = ["Ma Benz :)"];}
 	for (i=0; i<g.length; i++){$("music2").innerHTML += "<input type ='radio' name='artist' value='"+i+"'>"+g[i]+"<br>"};
 	$("music2").innerHTML += "<input type='button' name='playButton' value='°:. Play .:°' onclick=start('"+grou+"'); class='shifumiBou' id='playbut'>"
 };
